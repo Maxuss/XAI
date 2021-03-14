ed467c0b74350c0801c6589ccd931b36aaa099b8
Maksim <maxus.info.mail@gmail.com>
2021-03-14 20:22:09 +0300

    добавил кучу новых файлов для следующего релиза и обновил старые

---
+++ quests/quests.py
@@ -0,0 +1,22 @@
+import sys
+import json
+import os
+
+# QUESTDATA
+# INFO ABOUT QUESTS AND 
+# THEIR CONDITIONS
+# 
+# 
+# 
+# QUESTHUB BASICALLY
+
+import questline.q001 as q1  #  
+import questline.q002 as q2  # 
+import questline.q003 as q3  #  
+import questline.q004 as q4  #  
+import questline.q005 as q5  # 
+import questline.q006 as q6  # 
+import questline.q007 as q7  # 
+import questline.q008 as q8  # 
+import questline.q009 as q9  # 
+import questline.q010 as q10 # 
