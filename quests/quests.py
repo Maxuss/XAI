cb41d3b32c8a3f56e02f214f896dc5bbeac45a2a
Maksim <maxus.info.mail@gmail.com>
2021-03-14 20:24:32 +0300

    малый багфикс

--- quests/quests.py
+++ quests/quests.py
@@ -10,13 +10,13 @@ import os
 # 
 # QUESTHUB BASICALLY
 
-import questline.q001 as q1  #  
-import questline.q002 as q2  # 
-import questline.q003 as q3  #  
-import questline.q004 as q4  #  
-import questline.q005 as q5  # 
-import questline.q006 as q6  # 
-import questline.q007 as q7  # 
-import questline.q008 as q8  # 
-import questline.q009 as q9  # 
-import questline.q010 as q10 # 
+import questline.q001 as q1  # GET_AWAY_FROM_TOWN:1
+import questline.q002 as q2  # GET_AWAY_FROM_TOWN:2
+import questline.q003 as q3  # GET_AWAY_FROM_TOWN:3
+import questline.q004 as q4  # KILL_MONSTER
+import questline.q005 as q5  # LOOT_CHEST
+import questline.q006 as q6  # MEET_MORSHU_BROTHER:1
+import questline.q007 as q7  # MEET_MORSHU_BROTHER:2
+import questline.q008 as q8  # SURVIVE_SIEGE:1
+import questline.q009 as q9  # SURVIVE_SIEGE:2
+import questline.q010 as q10 # FILLER_TEST_IGNORE
